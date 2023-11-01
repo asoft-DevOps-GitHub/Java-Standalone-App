fa4df1c9d7ea6c5b1a6bee472c825e8b5f0f7c20
Ashok <Ashok@localhost.localdomain>
2023-11-01 12:47:35 +0530

    All files Commit

---
+++ Java-Standalone-App/Employee.java
@@ -0,0 +1,19 @@
+class Employee
+{
+	int empId=999;
+	String empName="Ashok";
+
+    
+
+public static void main(String[]args)
+{
+
+	Employee ee=new Employee();
+
+	System.out.println(ee.empId);
+	System.out.println(ee.empName);
+
+
+
+}
+
---
+++ Java-Standalone-App/EmployeeDB.java
@@ -0,0 +1,33 @@
+Class EmployeeDB
+{
+
+	int empId;
+	String empName;
+
+     void int display(int empno)
+     {
+	    int  empId=empno;
+
+	     return empId;
+        
+     }
+
+     void String screen(empNam)
+     {
+
+	    String  empName=empNam;
+
+	     return empName;
+     }
+
+
+     public Static void (String[]args)
+     {
+	     EmployeeDB edb= new Employee();
+
+	   int  empid = edb.display(999)
+           String empname = edb.screen("Ashok");
+     
+
+	   system.out.prinln(empid);
+	   System.out.println(empname);
